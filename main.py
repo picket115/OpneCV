27a33a1cbf408a9eff7dc3be20b5831aeca36102
PyoHyeIn <picket5774@gmail.com>
2023-03-30 17:26:04 +0900

    커밋2 - 그레이스케일 그레이스케일 기능 구현

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import cv2
 ## 영상 이미지 읽기
-src = cv2.imread('images/picture01.jpg')
+scr = cv2.imread('images/picture01.jpg')
 
+
+## 영상 처리 알고리즘 구현 ##
+dst1 = cv2.cvtColor(scr, cv2.COLOR_RGB2GRAY)
 
 
 
 
 ## 영상 디스플레이
-cv2.imshow('src', src)
+cv2.imshow('scr', scr)
+cv2.imshow('dest1', dst1)
+
 
 ## 마무리
 cv2.waitkey(0)
